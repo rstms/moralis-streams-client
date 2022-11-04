dcb3b23cb0a1ad45d5d72e34e939c6000b1c0cc3
Matt Krueger <mkrueger@rstms.net>
2022-11-04 10:18:44 -0500

    Bump version: 0.5.6 → 0.5.7

--- moralis_streams_client/version.py
+++ moralis_streams_client/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-11-04 10:17:34-05:00"
+__timestamp__ = "2022-11-04 10:18:44-05:00"
